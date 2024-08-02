2642497d795bba21c49fe5be0f8c1281dca237a4
vlk2022 <volodymyrkostiuk2021@gmail.com>
2024-08-02 23:18:13 +0300

    training

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,30 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { UsersComponent } from './components/users/users.component';
-import {HttpClientModule} from "@angular/common/http";
-import { UserComponent } from './components/user/user.component';
-import { PostsComponent } from './components/posts/posts.component';
-import { PostComponent } from './components/post/post.component';
-import { CommentsComponent } from './components/comments/comments.component';
-import { CommentComponent } from './components/comment/comment.component';
-import { UserDetailsComponent } from './components/user-details/user-details.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    UsersComponent,
-    UserComponent,
-    PostsComponent,
-    PostComponent,
-    CommentsComponent,
-    CommentComponent,
-    UserDetailsComponent,
   ],
   imports: [
     BrowserModule,
-    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
